e97a738406e261121a415031b5c1e1488f0c4c60
Musa SAYAR <musasayar67@gmail.com>
2023-11-02 13:12:02 +0300

    backend

---
+++ helpers/db.js
@@ -0,0 +1,17 @@
+// db.js
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const dbUrl = "mongodb://127.0.0.1:27017/activity"; 
+// const dbUrl = process.env.MONGODB_CONNECT_URI;// Your MongoDB connection URL
+
+mongoose.connect(dbUrl);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+module.exports = mongoose;
---
+++ server.js
@@ -0,0 +1,143 @@
+// server.js
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(bodyParser.json({ limit: "100mb" }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// Import the Mongoose connection
+const mongoose = require("./helpers/db"); // Adjust the path to your db.js file
+
+// Define your Mongoose schema and model for movies
+const activitySchema = new mongoose.Schema({
+  artist: String,
+  city: String,
+  description: String,
+  category: String,
+  image: [{ photo: String }],
+  location: String,
+  locationName: String,
+  hour: String,
+  date: String,
+});
+
+const Activity = mongoose.model("Activity", activitySchema);
+
+// Define your routes
+app.get("/api/activity", async (req, res) => {
+  const activity = await Activity.find({});
+  res.json({ activity });
+});
+
+app.post("/api/activity", async (req, res) => {
+  const {
+    artist,
+    city,
+    description,
+    category,
+    image,
+    location,
+    locationName,
+    hour,
+    date,
+  } = req.body;
+
+  const activity = new Activity({
+    artist,
+    city,
+    description,
+    category,
+    image,
+    location,
+    locationName,
+    hour,
+    date,
+  });
+
+  try {
+    const savedActivity = await activity.save();
+    res.json({ activity: savedActivity });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.put("/api/activity/:id", async (req, res) => {
+  const { id } = req.params;
+  const {
+    artist,
+    city,
+    description,
+    category,
+    image,
+    location,
+    locationName,
+    hour,
+    date,
+  } = req.body;
+
+  try {
+    const updatedActivity = await Activity.findByIdAndUpdate(
+      id,
+      {
+        artist,
+        city,
+        description,
+        category,
+        image,
+        location,
+        locationName,
+        hour,
+        date,
+      },
+      { new: true } // Return the updated document
+    );
+
+    if (updatedActivity) {
+      res.json({ activity: updatedActivity });
+    } else {
+      res.status(404).json({ error: "Activity not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.get("/api/activity/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const activity = await Activity.findById(id);
+    if (activity) {
+      res.json({ activity });
+    } else {
+      res.status(404).json({ error: "Activity not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.delete("/api/activity/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const activity = await Activity.findByIdAndDelete(id);
+    if (activity) {
+      res.json({ message: "Activity deleted successfully" });
+    } else {
+      res.status(404).json({ error: "Activity not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// Start the Express server
+const port = process.env.PORT || 5030;
+app.listen(port, () => {
+  console.log(`Server is running on localhost:${port}`);
+});
