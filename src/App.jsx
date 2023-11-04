f0272414d9ea8031d2ee2136ba7218aa2a0f9c8d
Kübra Emektar <emektarrkubra@gmail.com>
2023-11-04 16:37:16 +0300

    added reset.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 function App() {
   return (
-    <></>
+    <>
+    <h1>Home</h1>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import "./styles/reset.css"
 import "./styles/global.css"
 import { BrowserRouter } from 'react-router-dom'
 
