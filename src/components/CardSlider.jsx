e2cf2d7ef355d337ef97902c29aaa44a4df7007a
Kübra Emektar <emektarrkubra@gmail.com>
2023-11-08 13:11:03 +0300

    added

---
+++ src/components/CardSlider.jsx
@@ -0,0 +1,15 @@
+export default function CardSlider() {
+    return (<>
+        <div>
+            <a href="#">
+                <img class="rounded-t-lg w-48" src="https://www.bca.co.id/-/media/Feature/News/Edukatips/2022/09/20220912-tips-nonton-konser-pertama-kali-banner.jpg" alt="" />
+            </a>
+            <a href="#">
+                <img class="rounded-t-lg w-48" src="https://www.bca.co.id/-/media/Feature/News/Edukatips/2022/09/20220912-tips-nonton-konser-pertama-kali-banner.jpg" alt="" />
+            </a>
+            <a href="#">
+                <img class="rounded-t-lg w-48" src="https://www.bca.co.id/-/media/Feature/News/Edukatips/2022/09/20220912-tips-nonton-konser-pertama-kali-banner.jpg" alt="" />
+            </a>
+        </div>
+    </>)
+}
