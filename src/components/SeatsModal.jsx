577cfd9303ccf7febd738cb6f8a26dc6ad6ac2a0
Musa SAYAR <musasayar67@gmail.com>
2023-11-11 13:09:12 +0300

    detailpage

--- src/components/SeatsModal.jsx
+++ src/components/SeatsModal.jsx
@@ -34,8 +34,8 @@ export default function SeatsModal() {
 
     return (
         <>
-            <button onClick={() => setIsOpenModal(prev => !prev)} data-modal-target="default-modal" data-modal-toggle="default-modal" className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-                Toggle modal
+            <button onClick={() => setIsOpenModal(prev => !prev)} data-modal-target="default-modal" data-modal-toggle="default-modal" className="block text-white bg-blue-700 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center " type="button">
+             Select Armchair
             </button>
 
             <div id="default-modal" tabIndex="-1" aria-hidden="true" className={`flex items-center justify-center fixed top-0 left-0 right-0 z-50  ${isOpenModal ? `block` : `hidden`}  w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full`}>
