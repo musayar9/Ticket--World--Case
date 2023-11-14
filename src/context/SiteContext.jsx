7118c6a1a0205dffd2274a64e4541b99aa376ba2
Musa SAYAR <musasayar67@gmail.com>
2023-11-14 23:29:08 +0300

    loading and error components added

--- src/context/SiteContext.jsx
+++ src/context/SiteContext.jsx
@@ -3,13 +3,16 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { axiosConcertApi } from "../axios/axiosConcertApi";
+import Loading from "../components/Loading";
+import Error from "../components/Error";
 export const SiteContext = createContext();
 
 export default function SiteContextProvider({ children }) {
-
   const navigate = useNavigate();
-  const showSuccessToast = (message) => toast.success(message, { hideProgressBar: true, pauseOnHover: true });
-  const showErrorToast = (message) => toast.error(message, { hideProgressBar: true, pauseOnHover: true });
+  const showSuccessToast = (message) =>
+    toast.success(message, { hideProgressBar: true, pauseOnHover: true });
+  const showErrorToast = (message) =>
+    toast.error(message, { hideProgressBar: true, pauseOnHover: true });
 
   const [isSignup, setIsSignup] = useState(false); // for toast message
   const [isLogin, setIsLogin] = useState(false); // for toast message
@@ -19,10 +22,10 @@ export default function SiteContextProvider({ children }) {
   const [cartList, setCartList] = useState(); // cart
   const [sidebar, setSidebar] = useState(false); // sidebar
   const [isOpenModal, setIsOpenModal] = useState(false); // seat modal
-  const [isAvailableSelectedSeat, setIsAvailableSelectedSeat] = useState() // control of selectedSeat - true/false
+  const [isAvailableSelectedSeat, setIsAvailableSelectedSeat] = useState(); // control of selectedSeat - true/false
   const [selectedSeats, setSelectedSeats] = useState([]); // selectedSeat List
-  const [isOpenAvatarModal, setIsOpenAvatarModal] = useState()
-  const [avatarUrl, setAvatarUrl] = useState()
+  const [isOpenAvatarModal, setIsOpenAvatarModal] = useState();
+  const [avatarUrl, setAvatarUrl] = useState();
 
   const location = useLocation();
   const currentPathName = location.pathname;
@@ -31,37 +34,38 @@ export default function SiteContextProvider({ children }) {
   useEffect(() => {
     localStorage.removeItem("selectedSeats");
     setIsAvailableSelectedSeat(false);
-    setAvatarUrl(JSON.parse(localStorage.getItem("onlineUser"))?.avatar)
+    setAvatarUrl(JSON.parse(localStorage.getItem("onlineUser"))?.avatar);
   }, [location]);
-
 
   useEffect(() => {
     const storedOnlineUser = JSON.parse(localStorage.getItem("onlineUser"));
     if (storedOnlineUser?.id) {
       navigate(`${currentPathName}`);
       setIsValid(true);
-      setFavList([...JSON.parse(localStorage.getItem("onlineUser"))?.favorites])
-      setCartList([...JSON.parse(localStorage.getItem("onlineUser"))?.cart])
-      setAvatarUrl(JSON.parse(localStorage.getItem("onlineUser"))?.avatar)
+      setFavList([
+        ...JSON.parse(localStorage.getItem("onlineUser"))?.favorites,
+      ]);
+      setCartList([...JSON.parse(localStorage.getItem("onlineUser"))?.cart]);
+      setAvatarUrl(JSON.parse(localStorage.getItem("onlineUser"))?.avatar);
     } else {
       navigate("/login");
     }
   }, []);
 
   let totalCost = 0;
-  cartList?.forEach(concert => {
-    concert.selectedSeats.forEach(seat => {
+  cartList?.forEach((concert) => {
+    concert.selectedSeats.forEach((seat) => {
       const ticketPrice = Number(concert.item.ticketPrice);
       const rowIndex = seat.rowIndex;
       totalCost += ticketPrice * (8 / rowIndex);
     });
-  })
+  });
   const [concertData, setConcertData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
   const [error, setError] = useState();
   const [filteredToCategories, setFilteredToCategories] = useState();
-  const [isCategory, setIsCategory] = useState(true)
+  const [isCategory, setIsCategory] = useState(true);
   const [showPastEvents, setShowPastEvents] = useState(false);
   const getData = async () => {
     try {
@@ -69,8 +73,8 @@ export default function SiteContextProvider({ children }) {
       const responseData = response.data;
       if (response.status !== 200) {
         setIsError(true);
-        setError("Veri alınamadı");
-        throw new Error("Veri alınamadı");
+        setError("Data is not found");
+        throw new Error("Data is not found");
       }
       setConcertData(responseData.activity);
       setIsLoading(false);
@@ -78,13 +82,23 @@ export default function SiteContextProvider({ children }) {
         setFilteredToCategories(responseData.activity);
       }
     } catch (error) {
+      setIsError(true);
       setIsLoading(false);
+      setError(error);
       throw new Error(error);
     }
   };
   useEffect(() => {
     getData();
   }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (isError) {
+    return <Error message={error} />;
+  }
+
   return (
     <SiteContext.Provider
       value={{
@@ -117,9 +131,12 @@ export default function SiteContextProvider({ children }) {
         setIsCategory,
         showPastEvents,
         setShowPastEvents,
-        head, setHead,
-        isOpenAvatarModal, setIsOpenAvatarModal,
-        avatarUrl, setAvatarUrl
+        head,
+        setHead,
+        isOpenAvatarModal,
+        setIsOpenAvatarModal,
+        avatarUrl,
+        setAvatarUrl,
       }}
     >
       {children}
