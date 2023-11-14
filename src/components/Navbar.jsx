e2c2824e32183a8f8e1645f7bc90d0fc8e44340b
Ceren <ceren5262@gmail.com>
2023-11-14 12:10:36 +0300

    added search-bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,79 @@
-import React, { useState } from "react";
+import React, { useState, useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { NavLink } from "react-router-dom";
 import "../styles/navbar.css";
+import axios from 'axios';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
   const [showMediaIcons, setShowMediaIcons] = useState(false);
+  const [concertTitles, setConcertTitles] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/activity");
+        const data = response.data;
+        const titles = data.map((i) => i.title);
+        setConcertTitles(titles);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+  
+    fetchData();
+  }, []);
+
+
+  const handleAutocompleteChange = (event, value) => {
+    if (value) {
+      navigate("/city");
+    }
+  };
+
   return (
     <>
       <nav className="main-nav">
         <div className="logo">
-          <img src="/concert_logo.png" />
+          <img src="/concert_logo.png" alt="Logo"/>
         </div>
+        <div className="search-bar">
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={concertTitles}
+          onChange={handleAutocompleteChange}
+          getOptionLabel={(option) => option}
+          sx={{
+            width: 300,
+            '.MuiOutlinedInput-root': {
+              backgroundColor: 'white',
+              '&:hover': {
+                '& fieldset': {
+                  borderColor: 'white',
+                },
+              },
+              '&.Mui-focused': {
+                '& fieldset': {
+                  borderColor: 'white',
+                },
+              },
+            },
+            '.MuiInputBase-input': {
+              color: 'black',
+            },
+          }}
+          renderInput={(params) => 
+          <TextField 
+          {...params} 
+          label="Company Name" 
+          />}
+        />
+
+      </div>
         <div className={showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"}>
           <ul>
             <li>
