49cfad290757ee2cc8aedf240777dbbf368a0a79
Buse <busengokler@gmail.com>
2023-11-16 17:07:36 +0300

    Total cost added to payment page

---
+++ src/components/PaymentProgress.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PaymentProgress() {
+  return (
+    <div>PaymentProgress</div>
+  )
+}
+
+export default PaymentProgress
--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,4 +1,8 @@
-import React, {useState,useEffect} from 'react'
+import React, { useState, useContext } from "react";
+import { SiteContext } from "../context/SiteContext";
+import { formatPrice } from "../components/Functions";
+// import { FiInfo } from "react-icons/fi";
+import { ImInfo } from "react-icons/im";
 
 function Payment() {
   const [cardNumber, setCardNumber] = useState("");
@@ -6,6 +10,7 @@ function Payment() {
   const [cvv, setCVV] = useState("");
   const [submitted, setSubmitted] = useState(false);
   const [selectedPaymentOption, setSelectedPaymentOption] = useState(null);
+  const { totalCost } = useContext(SiteContext)
 
   //kart no uzunlugu
   const handleCardNumberChange = (e) => {
@@ -16,7 +21,7 @@ function Payment() {
 
   //cvv uzunlugu
   const handleCVVChange = (e) => {
-    if (e.target.value.length <= 6) {
+    if (e.target.value.length <= 3) {
       setCVV(e.target.value);
     }
   };
@@ -53,14 +58,16 @@ function Payment() {
           <li
             className={`flex w-full items-center text-blue-600 dark:text-blue-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-blue-100 after:border-4 after:inline-block dark:after:border-blue-800 ${
               submitted ? "text-gray-500 dark:text-gray-400" : ""
-            }`}>
+            }`}
+          >
             <div className="flex items-center justify-center w-10 h-10 bg-blue-100 rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0">
               <svg
                 className="w-4 h-4 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
-                viewBox="0 0 20 16">
+                viewBox="0 0 20 16"
+              >
                 <path d="M18 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2ZM6.5 3a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5ZM3.014 13.021l.157-.625A3.427 3.427 0 0 1 6.5 9.571a3.426 3.426 0 0 1 3.322 2.805l.159.622-6.967.023ZM16 12h-3a1 1 0 0 1 0-2h3a1 1 0 0 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Z" />
               </svg>
             </div>
@@ -115,8 +122,8 @@ function Payment() {
               <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">
                 Select Payment Method
               </label>
-              <div className="flex">
-                <button
+              <div className="flex ">
+                {/* <button
                   type="button"
                   className={`${
                     selectedPaymentOption === "troy"
@@ -126,17 +133,40 @@ function Payment() {
                   onClick={() => handlePaymentOptionChange("troy")}
                 >
                   Troy
-                </button>
+                </button> */}
                 <button
                   type="button"
                   className={`${
                     selectedPaymentOption === "mastercard"
                       ? "bg-blue-700"
                       : "bg-gray-300"
-                  } text-white font-medium rounded-lg text-sm px-3 py-2.5 mr-2 focus:outline-none`}
+                  } text-white font-medium rounded-lg text-sm px-1.5 py-1 mr-2 focus:outline-none`}
                   onClick={() => handlePaymentOptionChange("mastercard")}
                 >
-                  Mastercard
+                  <svg
+                    viewBox="0 0 38 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    width={38}
+                    height={24}
+                    aria-labelledby="pi-master"
+                  >
+                    <title id="pi-master">Mastercard</title>
+                    <path
+                      opacity=".07"
+                      d="M35 0H3C1.3 0 0 1.3 0 3v18c0 1.7 1.4 3 3 3h32c1.7 0 3-1.3 3-3V3c0-1.7-1.4-3-3-3z"
+                    />
+                    <path
+                      fill="#fff"
+                      d="M35 1c1.1 0 2 .9 2 2v18c0 1.1-.9 2-2 2H3c-1.1 0-2-.9-2-2V3c0-1.1.9-2 2-2h32"
+                    />
+                    <circle fill="#EB001B" cx={15} cy={12} r={7} />
+                    <circle fill="#F79E1B" cx={23} cy={12} r={7} />
+                    <path
+                      fill="#FF5F00"
+                      d="M22 12c0-2.4-1.2-4.5-3-5.7-1.8 1.3-3 3.4-3 5.7s1.2 4.5 3 5.7c1.8-1.2 3-3.3 3-5.7z"
+                    />
+                  </svg>
                 </button>
                 <button
                   type="button"
@@ -144,10 +174,31 @@ function Payment() {
                     selectedPaymentOption === "visa"
                       ? "bg-blue-700"
                       : "bg-gray-300"
-                  } text-white font-medium rounded-lg text-sm px-3 py-2.5 focus:outline-none`}
+                  } text-white font-medium rounded-lg text-sm px-1.5 py-1 focus:outline-none`}
                   onClick={() => handlePaymentOptionChange("visa")}
                 >
-                  Visa
+                  <svg
+                    viewBox="0 0 38 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    width={38}
+                    height={24}
+                    aria-labelledby="pi-visa"
+                  >
+                    <title id="pi-visa">Visa</title>
+                    <path
+                      opacity=".07"
+                      d="M35 0H3C1.3 0 0 1.3 0 3v18c0 1.7 1.4 3 3 3h32c1.7 0 3-1.3 3-3V3c0-1.7-1.4-3-3-3z"
+                    />
+                    <path
+                      fill="#fff"
+                      d="M35 1c1.1 0 2 .9 2 2v18c0 1.1-.9 2-2 2H3c-1.1 0-2-.9-2-2V3c0-1.1.9-2 2-2h32"
+                    />
+                    <path
+                      d="M28.3 10.1H28c-.4 1-.7 1.5-1 3h1.9c-.3-1.5-.3-2.2-.6-3zm2.9 5.9h-1.7c-.1 0-.1 0-.2-.1l-.2-.9-.1-.2h-2.4c-.1 0-.2 0-.2.2l-.3.9c0 .1-.1.1-.1.1h-2.1l.2-.5L27 8.7c0-.5.3-.7.8-.7h1.5c.1 0 .2 0 .2.2l1.4 6.5c.1.4.2.7.2 1.1.1.1.1.1.1.2zm-13.4-.3l.4-1.8c.1 0 .2.1.2.1.7.3 1.4.5 2.1.4.2 0 .5-.1.7-.2.5-.2.5-.7.1-1.1-.2-.2-.5-.3-.8-.5-.4-.2-.8-.4-1.1-.7-1.2-1-.8-2.4-.1-3.1.6-.4.9-.8 1.7-.8 1.2 0 2.5 0 3.1.2h.1c-.1.6-.2 1.1-.4 1.7-.5-.2-1-.4-1.5-.4-.3 0-.6 0-.9.1-.2 0-.3.1-.4.2-.2.2-.2.5 0 .7l.5.4c.4.2.8.4 1.1.6.5.3 1 .8 1.1 1.4.2.9-.1 1.7-.9 2.3-.5.4-.7.6-1.4.6-1.4 0-2.5.1-3.4-.2-.1.2-.1.2-.2.1zm-3.5.3c.1-.7.1-.7.2-1 .5-2.2 1-4.5 1.4-6.7.1-.2.1-.3.3-.3H18c-.2 1.2-.4 2.1-.7 3.2-.3 1.5-.6 3-1 4.5 0 .2-.1.2-.3.2M5 8.2c0-.1.2-.2.3-.2h3.4c.5 0 .9.3 1 .8l.9 4.4c0 .1 0 .1.1.2 0-.1.1-.1.1-.1l2.1-5.1c-.1-.1 0-.2.1-.2h2.1c0 .1 0 .1-.1.2l-3.1 7.3c-.1.2-.1.3-.2.4-.1.1-.3 0-.5 0H9.7c-.1 0-.2 0-.2-.2L7.9 9.5c-.2-.2-.5-.5-.9-.6-.6-.3-1.7-.5-1.9-.5L5 8.2z"
+                      fill="#142688"
+                    />
+                  </svg>
                 </button>
               </div>
             </div>
@@ -204,7 +255,7 @@ function Payment() {
                   name="expirationDate"
                   id="expirationDate"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                //   placeholder="MM/YYYY"
+                  //   placeholder="MM/YYYY"
                   required=""
                 />
               </div>
@@ -212,10 +263,11 @@ function Payment() {
               <div>
                 <label
                   htmlFor="cvv"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+                  className="inline-flex gap-x-1 mb-2 text-sm font-medium text-gray-900 dark:text-black"
                 >
-                  CVV
-                  <svg
+                  CVV <ImInfo/>
+                  
+                  {/* <svg
                     class="flex-shrink-0 inline w-4 h-4 me-3"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
@@ -223,7 +275,7 @@ function Payment() {
                     viewBox="0 0 20 20"
                   >
                     <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-                  </svg>
+                  </svg> */}
                 </label>
                 <input
                   type="number"
@@ -232,10 +284,17 @@ function Payment() {
                   value={cvv}
                   onChange={handleCVVChange}
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="0123"
+                  placeholder="It is the three-digit code printed on the back of the card."
                   required=""
                 />
               </div>
+            </div>
+            {/* Total Cost */}
+            <div className="my-2">
+              <hr className="border border-t-2" />
+              <h3 className="my-2 text-base font-bold text-center text-gray-700">TOTAL : <span>{`${formatPrice(totalCost.toFixed(2))}`}</span></h3>
+              <hr className="border border-b-2" />
+              
             </div>
             {/* checkbox */}
             <div>
@@ -248,15 +307,24 @@ function Payment() {
               <p>
                 <input type="checkbox" id="kredikayit" />
                 <label className="mx-2" htmlFor="kredikayit">
-                  I accept <span className='text-blue-700 font-semibold'>Information Notice Regarding Personal Data Processing</span> regarding the storage of my card.
+                  I accept{" "}
+                  <span className="text-blue-700 font-semibold cursor-pointer">
+                    Information Notice Regarding Personal Data Processing
+                  </span>{" "}
+                  regarding the storage of my card.
                 </label>
               </p>
             </div>
             {/* pay */}
             <button
               type="submit"
-              className=" my-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium
-                rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className={`my-3 text-black ${
+                !cardNumber || !nameOnCard || !cvv
+                  ? 'bg-gray-300 cursor-not-allowed'
+                  : 'bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300'
+              } font-medium rounded-lg text-sm px-5 py-2.5 text-center`}
+              disabled={!cardNumber || !nameOnCard || !cvv}
+              //  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
             >
               Confirm Payment
             </button>
@@ -267,5 +335,4 @@ function Payment() {
   );
 }
 
-
-export default Payment
+export default Payment;
