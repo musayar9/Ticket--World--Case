00fa379df7415fa1a0b1f1f1888e69523c9cf418
Musa SAYAR <musasayar67@gmail.com>
2023-11-29 15:10:16 +0300

    edit file

--- backend/models/City.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("../helpers/db");
-
-const citySchema = new mongoose.Schema({
-  plate: String,
-  name: String,
-});
-
-module.exports = mongoose.model("City", citySchema);
